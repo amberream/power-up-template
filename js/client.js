2eedb33b2b8ea2a7af0665a6c91bdca05a03d8bd
amberream <adream114@gmail.com>
2018-01-15 22:09:18 -0500

    Test5
    Test5

--- js/client.js
+++ js/client.js
@@ -253,7 +253,7 @@ var cardButtonCallback = function(t){
 var youTubeButtonCallback = function(t) {
     
     return t.popup({
-    title: 'Test4',
+    title: 'Test5',
 //    items: function (t, options) {
 //      // use options.search which is the search text entered so far
 //      // return a Promise that resolves to an array of items
@@ -274,9 +274,7 @@ var youTubeButtonCallback = function(t) {
       // use options.search which is the search text entered so far
       // return a Promise that resolves to an array of items
       // similar to the items you provided in the client side version above
-      return new Promise(function (resolve) {
-        // you'd probably be making a network request at this point
-         
+      
         var response = $.ajax({
             url: "https://www.googleapis.com/youtube/v3/search",
             data: {
@@ -295,29 +293,23 @@ var youTubeButtonCallback = function(t) {
         });
           
         // when the response is finished, then return a list of items
-        return response.then(function(){
-//            resolve(items);
-            
-          resolve([{
+        return response.then(function(){            
+          return [{
             text: 'Result 1',
             callback: function (t, opts) { }
           }, {
             text: 'Result 2',
             callback: function (t, opts) { }
-          }]);   
-        
-        });
-                
-      });
-               
+          }]
+        });        
     },  
     search: {
       // optional # of ms to debounce search to
       // defaults to 300, override must be larger than 300
       debounce: 300,
-      placeholder: 'Test4 Placeholder',
-      empty: 'Test4 Empty',
-      searching: 'Test4 Searching'
+      placeholder: 'Test5 Placeholder',
+      empty: 'Test5 Empty',
+      searching: 'Test5 Searching'
     }
   });
     
