bfd7df2c3af55ff9c7e266acdcde6dd936e57917
amberream <adream114@gmail.com>
2018-01-17 19:52:58 -0500

    Access Local Variable in Callback
    Callback should be a function that returns a function so we can access
    the local variable

--- js/client.js
+++ js/client.js
@@ -290,13 +290,14 @@ var youTubeButtonCallback = function (t) {
                 for (var i = 0; i < items.length; i++) {
                     ret.push({
                         text: items[i].snippet.title,
-                        callback: function (t, opts) {
-                            return t.attach({
-                                name: items[i].snippet.title, // optional
-                                url: "https://www.youtube.com/watch?v=" + items[i].id.videoId // required
-                            });
-                        },
-                        url: "https://www.youtube.com/watch?v=" + items[i].id.videoId
+                        callback: (function (item) {
+                            return function (t, opts) {
+                                return t.attach({
+                                    name: item.snippet.title, // optional
+                                    url: "https://www.youtube.com/watch?v=" + item.id.videoId // required
+                                });
+                            }
+                        })(items[i])
                     });
                 }
                 console.log(ret);
