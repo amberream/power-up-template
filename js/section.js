275f7b19f3914970d30fcf3d6572e0a9a75a69bc
amberream <adream114@gmail.com>
2018-03-29 20:55:16 -0400

    Fullscreen modal on click

--- js/section.js
+++ js/section.js
@@ -16,7 +16,7 @@ t.render(function () {
         })
         .then(function (youtubeAttachments) {
             var urls = youtubeAttachments.map(function (a) {
-                return '<div class="video"><iframe src="' + a.url + '?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
+                return '<div class="video" onclick="modal(a.url)"><iframe src="' + a.url + '?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
             });
             document.getElementById('videos').innerHTML = urls.join(' ');
         })
@@ -24,3 +24,10 @@ t.render(function () {
             return t.sizeTo('#content');
         });
 });
+
+function modal(url) {
+    t.modal({
+        url: url,
+        fullscreen: true
+    });
+}
